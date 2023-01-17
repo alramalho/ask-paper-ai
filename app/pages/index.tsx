647ccc576a00549ac56a34cd44aeb0a67ae2df05
alramalho <alexandre.ramalho.1998@gmail.com>
2023-01-17 12:58:27 +0100

    v3

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,199 +1,144 @@
-import {NextPage} from "next";
-import {useEffect, useState} from "react";
-import {Box, Layout} from "../components/layout";
-import {Button, Loading, Progress, Radio, Spacer, Text} from "@nextui-org/react";
-import MarkdownView from 'react-showdown';
+import { Text, Button, Card, Spacer, Loading, Input, Textarea, useInput } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import MarkdownView from "react-showdown";
+import UploadIcon from "../components/icons/upload-icon";
+import SendIcon from "../components/icons/send-icon";
+import { Box } from "../components/layout";
+import { Flex } from "../components/styles/flex";
 
 type Paper = {
-  abstract: string
-  pdf_parse: {
-    body_text: {
-      text: string
-      section: string
-    }[],
-    back_matter: {
-      text: string
-      section: string
-    }[]
-  }
+    abstract: string
+    pdf_parse: {
+        body_text: {
+            text: string
+            section: string
+        }[],
+        back_matter: {
+            text: string
+            section: string
+        }[]
+    }
 }
 
-const Home: NextPage = () => {
-  const [LLMResponse, setLLMResponse] = useState<string | undefined>(undefined)
-  const [seconds, setSeconds] = useState(0);
-  const [isRunning, setIsRunning] = useState<boolean>(false)
-  const [loadingText, setLoadingText] = useState<string | undefined>(undefined)
+const Temp = () => {
+    const [LLMResponse, setLLMResponse] = useState<string | undefined>(undefined)
+    const [seconds, setSeconds] = useState(0);
+    const [isRunning, setIsRunning] = useState<boolean>(false)
+    const [loadingText, setLoadingText] = useState<string | undefined>(undefined)
+    const [selectedPaperName, setSelectedPaperName] = useState<string | undefined>(undefined)
 
-  const [papers, setPapers] = useState<string[] | undefined>(undefined)
-  const [selectedPaper, setSelectedPaper] = useState<Paper | undefined>(undefined)
-  const [selectedPaperName, setSelectedPaperName] = useState<string | undefined>(undefined)
-
-  useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_PDF2JSON_APIURL}/get-papers`).then(response => response.json()).then(
-      json => setPapers(json)
-    )
-  }, [])
-
-  async function getPaper(path: string) {
-    let paper: Paper = await fetch(`${process.env.NEXT_PUBLIC_PDF2JSON_APIURL}/get-paper?path=${path}`).then(response => response.json());
-    setSelectedPaper(paper)
-    return paper
-  }
-
-  useEffect(() => {
-    let intervalId: any = null;
-    if (isRunning) {
-      intervalId = setInterval(() => {
-        setSeconds(seconds => seconds + 1);
-      }, 1000);
-    } else if (seconds !== 0) {
-      clearInterval(intervalId);
-    }
-    return () => clearInterval(intervalId);
-  }, [isRunning, seconds]);
-
-  const handleSubmit = async () => {
-    setSeconds(0)
-    setIsRunning(true)
-    setLoadingText("Reading paper abstract...")
-    const paperPath = await fetch(`${process.env.NEXT_PUBLIC_PDF2JSON_APIURL}/parse-paper?name=${selectedPaperName}`).then(response => response.json())
-    const paper = await getPaper(paperPath)
-    console.log(selectedPaperName)
-    console.log("Abstract: ")
-    console.log(paper.abstract);
+    const [papers, setPapers] = useState<string[] | undefined>(undefined)
+    const {
+        value: questionValue,
+        setValue: setQuestionValue,
+        reset: resetQuestion,
+        bindings,
+    } = useInput("Type your question here...");
 
 
-    const responses = [await extractDatasets(paper.abstract)]
-    setLLMResponse(responses[0])
+    useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_PDF2JSON_APIURL}/get-papers`).then(response => response.json()).then(
+            json => setPapers(json)
+        )
+    }, [])
 
-    for (const bodyText of paper.pdf_parse.body_text.concat(paper.pdf_parse.back_matter)) {
-      if (bodyText.section.toLowerCase().includes('data')) {
-        console.log(bodyText.section + ": ")
-        console.log(bodyText.text)
-        setLoadingText(`Reading ${bodyText.section} section...`)
-        const response = await extractDatasets(bodyText.text)
-        responses.push(response)
-      }
-      setLLMResponse(responses.join('\n'))
+    async function getPaper(path: string) {
+        let paper: Paper = await fetch(`${process.env.NEXT_PUBLIC_PDF2JSON_APIURL}/get-paper?path=${path}`).then(response => response.json());
+        return paper
     }
 
-    setLoadingText(`Summarizing the findings...`)
-    const tableSummary = await toTable(responses.join('\n'))
-    setLLMResponse(tableSummary)
+    const handleSubmit = async (paperName: string, question: string, sectionFilterer: (sectionName:string) => boolean = (sectionName:string) => true) => {
+        setSeconds(0)
+        setIsRunning(true)
+        setLoadingText("Reading paper...")
+        const paperPath = await fetch(`${process.env.NEXT_PUBLIC_PDF2JSON_APIURL}/parse-paper?name=${paperName}`).then(response => response.json())
+        const paper = await getPaper(paperPath)
 
-    setLoadingText(`Injecting Source URLs...`)
-    setLLMResponse(await updateMarkdownTableWithSourceURLs(tableSummary))//, "RibFrac Dataset", "Source URL", `<a href="${datasetSource}" target="_blank">${datasetSource}</a>`))
-    console.log(LLMResponse)
+        const aggreggatedText = paper.abstract + paper.pdf_parse.body_text.concat(paper.pdf_parse.back_matter).filter(e => sectionFilterer(e.section)).map(bodyText => bodyText.text).join('\n')
 
-    setIsRunning(false)
-  }
-
-  return (
-    <Layout css={{flexDirection: 'row'}}>
-      {papers
-        ? <>
-          <Radio.Group label="Options" onChange={setSelectedPaperName}>
-            {papers.map((elem, index) => <Radio key={elem} value={elem}>{elem}</Radio>)}
-          </Radio.Group>
-          <Spacer y={3}/>
-          <Button color="gradient" onClick={handleSubmit}>Extract Datasets</Button>
-          <Spacer y={2}/>
-        </>
-        : "No papers"
-      }
-      <Text h4>Time elapsed: {seconds} seconds</Text>
-      {isRunning
-        && <>
-              <Loading type="points">{loadingText}</Loading>
-          </>
-      }
-      <br/>
-      {LLMResponse &&
-          <Box css={{textAlign: 'left', margin: '$6'}}>
-              <MarkdownView
-                  markdown={LLMResponse}
-                  options={{tables: true, emoji: true,}}
-              />
-          </Box>
-      }
-
-    </Layout>
-  )
-}
-
-
-async function extractDatasets(text: string) {
-  return (await fetch(`${process.env.NEXT_PUBLIC_CHATGPT_PYTHON_APIURL}/ask`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      text: `Extract the datasets and their characteristics in a nested list manner from the following text: ${text}`,
-      // text: `Extract the datasets and their characteristics – include "probable source url" as a characteristic – in a nested list manner from the following text: ${text}`,
-    })
-  }).then(response => response.json()).catch(console.error)).message;
-}
-
-
-async function toTable(text: string) {
-  return (await fetch(`${process.env.NEXT_PUBLIC_CHATGPT_PYTHON_APIURL}/ask`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      text: `Please summarize the following text, bounded by "START" and "END", on a markdown table. The text will contain possibly repeated information about the charactersitics of one or more datasets. I want you to summarize the whole text into a markdown table that represents the characterstics of all the datasets. The resulting table should be easy to read and contain any information that might be useful for people thinking about using any of those datasets. Some example fields would be "Name", "Size", "Demographic information" and "Origin", but add as many as you think are relevant for a medical researcher. The resulting table should contain as many entries as possible but it should NOT contain any duplicates (columns with the same "Name" field) and it should NOT contain any entries where the "Name" field is not defined/unkown/ not specified.\nSTART\N${text}\nEND`,
-      // text: `Extract the datasets and their characteristics in a nested list manner from the following text: ${text}`,
-    })
-  }).then(response => response.json()).catch(console.error)).message;
-}
-
-async function searchDataset(dataset: string) {
-  return (await fetch(`${process.env.NEXT_PUBLIC_CHATGPT_PYTHON_APIURL}/search`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: dataset
-    })
-  }).then(response => response.json()).catch(() => "couldn't find dataset")).result;
-}
-
-async function updateMarkdownTableWithSourceURLs(tableString: string) {
-  tableString = tableString.split('\n\n')[0]
-  let lines = tableString.split("\n");
-  let headers = lines[0].split("|").map(s => s.trim());
-  let dividers = lines[1].split("|").map(s => s.trim());
-  const fieldToAdd = "Source URL"
-  let fieldToAddIndex = headers.indexOf(fieldToAdd);
-
-  if (fieldToAddIndex === -1) {
-    headers.push(fieldToAdd);
-    dividers.push('---');
-    lines[0] = headers.join(" | ");
-    lines[1] = dividers.join(" | ");
-    for (let i = 1; i < lines.length; i++) {
-      let cells = lines[i].split("|").map(s => s.trim());
-      cells.push("");
-      lines[i] = cells.join(" | ");
+        const datasetTable = await askPaper(question, aggreggatedText)
+        setLLMResponse(makeLinksClickable(datasetTable))
+        setIsRunning(false)
     }
-    fieldToAddIndex = headers.length - 1;
-  }
 
-  for (let i = 2; i < lines.length; i++) {
-    let cells = lines[i].split("|").map(s => s.trim());
-    const datasetSource = await searchDataset(cells[1]);
-    if (datasetSource != null) {
-      cells[fieldToAddIndex] = `<a href="${datasetSource}" target="_blank">${datasetSource}</a>`
-    } else {
-      cells[fieldToAddIndex] = `<span>couldn't find dataset link.</span>&nbsp;<a target="__blank" href='https://www.google.com/search?q=${cells[1]}'>search google instead</a>`
-    }
-    lines[i] = cells.join(" | ");
-  }
+    return <Flex justify="center" align="center" css={{ minHeight: '100%' }} direction="column">
+        <h2>Ask Paper</h2>
+        <Button icon={<UploadIcon />}>Upload your paper</Button>
+        <Spacer y={3} />
+        <h4>Or start with a template:</h4>
+        {papers && <Flex css={{ gap: "$2" }}> {papers.map(paper => <Card
+            isPressable
+            isHoverable
+            css={{ mw: "150px", mh: "200px", border: selectedPaperName === paper ? "1px solid blue" : undefined }}
+            variant="bordered"
+            onPress={() => setSelectedPaperName(paper)}
+        >
+            <Card.Body>
+                <Text>{paper}</Text>
+            </Card.Body>
+        </Card>)}</Flex>}
+        {selectedPaperName && <>
+            <Spacer y={3} />
+            <h4>And ask your question</h4>
+            <Flex css={{ gap: "$5" }}>
 
-  return lines.join("\n");
+                <Textarea
+                    {...bindings}
+                    fullWidth
+                    size="lg"
+                    minRows={4}
+                    maxRows={20}
+                    css={{ width: "400px", }}
+                />
+                <Button iconRight={<SendIcon />} onPress={() => handleSubmit(selectedPaperName, questionValue)} > Ask </Button>
+            </Flex>
+            <Spacer y={2} />
+            <h4>Or start with predefined action:</h4>
+            <Button
+                css={{backgroundColor: "$purple300", color: "black"}}
+                onPress={() => {
+                    handleSubmit(
+                        selectedPaperName, 
+                        `Please summarize the following text on a markdown table. The text will contain possibly repeated information about the charactersitics of one or more datasets. I want you to summarize the whole text into a markdown table that represents the characterstics of all the datasets. The resulting table should be easy to read and contain any information that might be useful for medical researchers thinking about using any of those datasets. Some example fields would be "Name", "Size", "Demographic information", "Origin" and "Data or code link to find more", but add as many as you think are relevant for a medical researcher. The resulting table should contain as many entries as possible but it should NOT contain any duplicates (columns with the same "Name" field) and it should NOT contain any entries where the "Name" field is not defined/unknown/ not specified.`,
+                        (e) => e.toLowerCase().includes('data'))
+                    }}
+            >
+                <Text>Extract datasets</Text>
+            </Button>
+                    <Spacer y={4} />
+                    <h3>Answer:</h3>
+            {isRunning
+                && <>
+                    <Loading type="points">{loadingText}</Loading>
+                </>
+            }
+            {LLMResponse &&
+                <Box css={{ textAlign: 'left', margin: '$6' }}>
+                    <MarkdownView
+                        markdown={LLMResponse}
+                        options={{ tables: true, emoji: true, }}
+                    />
+                </Box>
+            }
+        </>}
+
+    </Flex>;
+};
+
+async function askPaper(question: string, context: string) {
+    return (await fetch(`${process.env.NEXT_PUBLIC_CHATGPT_PYTHON_APIURL}/ask`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            text: `Please answer the following request, denoted by "Request:" in the best way possible with the given context that bounded by "Start context" and "End context".\Request: ${question}\nStart context\n${context}\nEnd context`,
+        })
+    }).then(response => response.json()).catch(console.error)).message;
 }
 
-export default Home;
+function makeLinksClickable(text: string) {
+    return text.replace(/(https?:\/\/[^\s]+)/g, "<a target=\"__blank\" href='$1'>$1</a>");
+}
+
+export default Temp;
--- chatgpt-api-python/main.py
+++ chatgpt-api-python/main.py
@@ -1,27 +1,22 @@
+import json
 import os.path
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request
 import os
 from fastapi.middleware.cors import CORSMiddleware
-from revChatGPT.ChatGPT import Chatbot
 from dotenv import load_dotenv
-from transformers import pipeline
 import logging
 import datetime
 import time
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
+import openai
 
-logging.basicConfig(filename=os.path.join(os.getcwd(), 'test.log'), level=logging.CRITICAL, force=True)
-
-import json
 load_dotenv()
+openai.api_key = os.getenv("OPENAI_KEY")
+logging.basicConfig(filename=os.path.join(
+    os.getcwd(), 'test.log'), level=logging.CRITICAL, force=True)
 
-logging.critical('\n---\nStarting up with date: ' + str(datetime.datetime.now()))
+
+logging.critical('\n---\nStarting up with date: ' +
+                 str(datetime.datetime.now()))
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -30,21 +25,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-chatbot = Chatbot({
-    "email": os.getenv("MICROSOFT_EMAIL"),
-    "password": os.getenv("MICROSOFT_PASSWORD"),
-    "isMicrosoftLogin": True
-}, conversation_id=None, parent_id=None)
 
 driver = None
 
-@app.post("/get-paper")
-async def getpapers(request: Request):
-    body = await request.json()
-    path = body["path"]
-    logging.critical('\n---\nGot paper at: ' + path)
-    with open(path, "r") as f:
-        return json.load(f)
 
 def write_to_json_cache(key, value):
     if not os.path.exists('cache.json'):
@@ -59,6 +42,7 @@ def write_to_json_cache(key, value):
         with open('cache.json', 'w') as f:
             json.dump(data, f)
 
+
 def read_from_json_cache(key):
     if not os.path.exists('cache.json'):
         return None
@@ -69,6 +53,16 @@ def read_from_json_cache(key):
         else:
             return None
 
+
+@app.post("/get-paper")
+async def getpapers(request: Request):
+    body = await request.json()
+    path = body["path"]
+    logging.critical('\n---\nGot paper at: ' + path)
+    with open(path, "r") as f:
+        return json.load(f)
+
+
 @app.post("/ask")
 async def ask(request: Request):
     body = await request.json()
@@ -77,40 +71,17 @@ async def ask(request: Request):
     logging.critical('\n---\nAsked text: \n' + text)
     if (read_from_json_cache(text) is not None):
         print("cache hit")
-        time.sleep(0.5)  
+        time.sleep(0.5)
         response = read_from_json_cache(text)
     else:
-        response = chatbot.ask(text, conversation_id=None, parent_id=None)
-        response = response["message"]
+        response = openai.Completion.create(
+            prompt=text,
+            # We use temperature of 0.0 because it gives the most predictable, factual answer.
+            temperature=0,
+            max_tokens=500,
+            model="text-davinci-002",
+        )["choices"][0]["text"].strip("\n")
         write_to_json_cache(text, response)
-        print(response)
+        
     logging.critical('\n---\Answer: \n' + response)
     return {"message": response}
-
-MAX_RETRIES = 3
-@app.post("/search")
-async def search(request: Request):
-    global driver
-    if driver is None:
-        driver = webdriver.Chrome()
-    try:
-        body = await request.json()
-        name = body["name"]
-
-        driver.get(f"https://datasetsearch.research.google.com/search?query={name}")
-        driver.implicitly_wait(0.5)
-        a_element = driver.find_element("xpath", "//div[contains(text(), \"Explore at:\")]//ul//li[1]//a")
-
-        result = a_element.get_attribute("href")
-        return {"result": result}
-    except WebDriverException as e:
-        if "Session not found" in str(e) or "invalid session id" in str(e):
-            driver.quit()
-            driver = webdriver.Chrome()
-            search(request)
-        elif "no such element" in str(e):
-            return {"result": None}
-        else:
-            raise e
-    except Exception as error:
-        return Response(content=f"Internal Server Error: {error}", status_code=500)
