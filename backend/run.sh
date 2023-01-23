a9a55cd33ae98d2d0cc1365c08d06ce0393bd33c
alramalho <alexandre.ramalho.1998@gmail.com>
2023-01-23 15:18:56 +0100

    Embed run_grobid.sh into whole backend run script

---
+++ backend/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+if [ -d $HOME/grobid-0.6.2 ]; then
+    echo "Grobid exists"
+else
+    echo "Grobid does not exist, installing..."
+    wget https://github.com/kermitt2/grobid/archive/0.6.2.zip
+    unzip 0.6.2.zip
+    mv grobid-0.6.2 $HOME
+    cd $HOME/grobid-0.6.2
+    ./gradlew install
+fi
+
+pushd $HOME/grobid-0.6.2
+./gradlew run &
+popd
+
+uvicorn main:app --reload --port 8000 &
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
--- backend/scripts/run_grobid.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd $HOME/grobid-0.6.2 || exit 1
-
-## Start Grobid
-./gradlew run
