6520aea4d32905562c9c83965c9df3b4336c4bd0
alramalho <alexandre.ramalho.1998@gmail.com>
2023-01-25 15:49:08 +0100

    Refactor build script to run in ubuntu:latest (gh actions)

--- backend/scripts/build.sh
+++ backend/scripts/build.sh
@@ -4,6 +4,8 @@ BLUE='\033[0;34m'
 GREEN='\033[0;32m'
 NOCOLOR='\033[0m'
 
+set -e
+
 echo $PWD
 echo "${GREEN}Started building backend zip 🧹${NOCOLOR}"
 echo "${BLUE}Cleaning up... 🧹${NOCOLOR}"
@@ -11,17 +13,14 @@ rm -rf ../src_dependencies && rm -rf ../build.zip
 
 echo "${BLUE}Building dependencies... 📦${NOCOLOR}"
 # it uses docker so dependencies are build from a linux image, resembling lambda real environment
-pushd ..
+cd ..
 docker run -v "$PWD":/var/task "lambci/lambda:build-python3.8" /bin/sh -c "pip install -r requirements.txt -t src_dependencies/; exit"
-pushd ./src_dependencies
+cd ./src_dependencies
 zip ../build.zip -r .
-popd
-popd
+cd ..
 
-pushd ..
 echo "${BLUE}Adding source code... 🌊${NOCOLOR}"
 cd ./src
 zip ../build.zip -u . -r
-popd
 
 echo "${GREEN}Done ✅${NOCOLOR}"
