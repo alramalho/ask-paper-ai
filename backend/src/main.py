fd7db98b4d70a8d021155af4de8ea45d0579cabb
alramalho <alexandre.ramalho.1998@gmail.com>
2023-02-06 12:52:04 +0100

    Add was_cut to logging mechanism

--- backend/src/main.py
+++ backend/src/main.py
@@ -186,6 +186,7 @@ def num_tokens(text):
 @app.post("/ask")
 async def ask(request: Request):
     start = datetime.datetime.now()
+    was_cut = False
     try:
         body = await request.json()
         question = body["question"]
@@ -194,6 +195,7 @@ async def ask(request: Request):
         text = f"Please answer the following request, denoted by \"Request:\" in the best way possible with the given paper context that bounded by \"Start paper context\" and \"End paper context\". Everytime \"paper\" is mentioned, it is referring to paper context denoted by \"Start paper context\" and \"End paper context\". {quote and 'You must always pair your response with a quote from the provided paper (and enclose the extracted quote between double quotes). The only time where you may not provide a quote is when the provided paper context does not contain any helpful information to the request presented, in this scenario, you must asnwer with a sentence saying `The paper does not contain enough information to answer your question`'}. Request: {question}\nStart paper context\n{context}\nEnd paper context"
         print(f"Asked text: \n{text}\n")
         if num_tokens(text) > 3500:
+            was_cut = True
             print("Text too long, was cut")
             max_length = int(len(' '.join(text.split(' ')) * 3500) / num_tokens(text))
             text = text[:max_length] + "\nEnd paper context"
@@ -216,6 +218,7 @@ async def ask(request: Request):
             'latest_commit_id': LATEST_COMMIT_ID,
             'time_elapsed': str(time_elapsed),
             'prompt_text': text,
+            'was_prompt_cut': was_cut,
             'response_text': response,
         })
 
