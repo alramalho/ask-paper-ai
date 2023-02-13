ef4a1d905dab3818507a715518aee28fe39c2814
alramalho <alexandre.ramalho.1998@gmail.com>
2023-02-13 18:47:26 +0100

    Refactor discord js backend api endpoint not to give ERR_HTTP_HEADERS_SENT error on next

--- frontend/pages/api/discord/[role].js
+++ frontend/pages/api/discord/[role].js
@@ -1,20 +1,27 @@
-const { Client, Events, GatewayIntentBits, GuildMember } = require('discord.js');
+const {Client, Events, GatewayIntentBits} = require('discord.js');
 export default async function handler(request, response) {
     const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers]});
-    client.login(process.env.DISCORD_CLIENT_BOT_TOKEN);
 
-    client.once(Events.ClientReady, async () => {
-        console.log("Client Ready!");
-        const guild = client.guilds.cache.get(process.env.DISCORD_SERVER_ID);
-        const members = await guild.members.fetch()
-        const hasRole = members.find(member => {
-            return member.roles.cache.find(role => role.name === request.query.role && member.id === request.query.userId)
-        })
+    await client.login(process.env.DISCORD_CLIENT_BOT_TOKEN);
+    const isWhitelisted = new Promise((resolve, reject) => {
+        client.once(Events.ClientReady, async () => {
+            console.log("Client Ready!");
+            const guild = client.guilds.cache.get(process.env.DISCORD_SERVER_ID);
+            const members = await guild.members.fetch()
+            const hasRole = members.find(member => {
+                return member.roles.cache.find(role => role.name === request.query.role && member.id === request.query.userId)
+            })
+            resolve(hasRole);
+        });
+        setInterval(() => {
+            reject();
+        }, 30000)
+    })
+
+    isWhitelisted.then(hasRole => {
         response.status(200).json({hasRole: hasRole !== undefined});
-    });
-
-    setInterval(() => {
-        response.status(500).json({message: 'Timeout'})
-    }, 35000)
+    }).catch(() => {
+        response.status(504).json({message: 'Timeout'})
+    })
 
 }
