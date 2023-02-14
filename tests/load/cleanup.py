64d306d6a8c49ef9df4202847d870cf72933f563
alramalho <alexandre.ramalho.1998@gmail.com>
2023-02-14 10:03:31 +0100

    Add cleanup.py to artillery tests

--- tests/load/cleanup.py
+++ tests/load/cleanup.py
@@ -3,3 +3,31 @@
 
 # just setup a project. Try poetry since you're at it
 # just rember this MUST RUN IN GITHUB ACTIONS
+
+import boto3
+
+# Create a DynamoDB client
+dynamodb_resource = boto3.resource('dynamodb')
+dynamodb_client = boto3.client('dynamodb')
+
+# Define the table names
+table_names = ['HippoPrototypeFeedback-sandbox', 'HippoPrototypeFunctionInvocations-sandbox', 'HippoPrototypeJsonPapers-sandbox']
+
+
+# Delete all items in each table
+for table_name in table_names:
+    print(f"Deleting all entries from {table_name}.")
+    table = dynamodb_resource.Table(table_name)
+    response = dynamodb_client.scan(TableName=table_name)
+    items = response.get('Items', [])
+    while items:
+        with table.batch_writer() as batch:
+            for item in items:
+                item = {'id': item.get('id').get('S')}
+                print(item)
+                batch.delete_item(Key=item)
+                if response.get('LastEvaluatedKey'):
+                    response = dynamodb_client.scan(TableName=table_name, ExclusiveStartKey=response.get('LastEvaluatedKey'))
+                else:
+                    response = dynamodb_client.scan(TableName=table_name)
+        items = response.get('Items', [])
