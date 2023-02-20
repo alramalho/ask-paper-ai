f1b3e76f4ed2b36adcfe59c9a6246fba32c559a0
alramalho <alexandre.ramalho.1998@gmail.com>
2023-02-20 16:19:35 +0100

    Use paper hash as name in s3 storage

--- backend/src/api.py
+++ backend/src/api.py
@@ -86,7 +86,7 @@ async def upload_paper(pdf_file: UploadFile, request: Request, background_tasks:
         'email': email,
         'paper_hash': paper_hash,
     })
-    background_tasks.add_task(store_paper_in_s3, pdf_file_content, pdf_file_name)
+    background_tasks.add_task(store_paper_in_s3, pdf_file_content, paper_hash)
 
     json_paper['id'] = paper_hash
 
