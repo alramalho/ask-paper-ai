58c57273d7c77bdd766b1447519be7513f8f6951
alramalho <alexandre.ramalho.1998@gmail.com>
2023-02-21 09:12:26 +0100

    Allow debugging

--- backend/src/api.py
+++ backend/src/api.py
@@ -177,4 +177,4 @@ async def store_feedback(request: Request):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
