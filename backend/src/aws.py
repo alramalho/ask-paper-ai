daf5dd551489e1cd51fea05f84663c926eb80d7d
alramalho <alexandre.ramalho.1998@gmail.com>
2023-02-27 19:21:34 +0100

    Refactor storing in s3

--- backend/src/aws.py
+++ backend/src/aws.py
@@ -9,12 +9,16 @@ LATEST_COMMIT_ID = os.getenv("LATEST_COMMIT_ID", 'local')
 
 
 def store_paper_in_s3(pdf_file: bytes, pdf_file_name: str):
-    print('Storing paper in S3')
     if ENVIRONMENT not in ['production', 'sandbox']:
+        print("Not storing paper in S3 because not in production or sandbox")
         return
 
+    print('Storing paper in S3')
     s3 = boto3.resource('s3')
-    s3.Bucket(S3_BUCKET_NAME).put_object(Key=f"papers/{pdf_file_name}.pdf", Body=pdf_file)
+    if '.pdf' not in pdf_file_name:
+        pdf_file_name = f'{pdf_file_name}.pdf'
+
+    s3.Bucket(S3_BUCKET_NAME).put_object(Key=f"papers/{pdf_file_name}", Body=pdf_file)
 
 
 def write_to_dynamo(table_name: str, data: dict):
