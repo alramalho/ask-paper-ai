5cdde7dcdbe8a6d50c7040d67bec5491520572e3
alramalho <alexandre.ramalho.1998@gmail.com>
2023-03-17 21:34:36 +0100

    Small refactor backend building

--- infrastructure/src/api-stack.ts
+++ infrastructure/src/api-stack.ts
@@ -24,7 +24,7 @@ export class ApiStack extends cdk.Stack {
 
         this.fastApiLambda = new lambda.Function(this, 'FastAPILambda', {
             functionName: `${CAMEL_CASE_PREFIX}FastAPI${props.environment}`,
-            code: lambda.Code.fromAsset(path.join(__dirname, '..', 'build.zip')),
+            code: lambda.Code.fromAsset(path.join(__dirname, '..', '..', 'backend', 'build.zip')),
             handler: 'api.handler',
             runtime: lambda.Runtime.PYTHON_3_8,
             timeout: cdk.Duration.seconds(150),
