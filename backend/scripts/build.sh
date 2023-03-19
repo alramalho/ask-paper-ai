5ab48153d20bfbc45963b40ac2bfd0ac4ce78a8a
alramalho <alexandre.ramalho.1998@gmail.com>
2023-03-19 13:43:15 +0100

    Refactor build
    Co-authored-by: Sheng Wang <shenggwangg@gmail.com>

--- backend/scripts/build.sh
+++ backend/scripts/build.sh
@@ -15,14 +15,14 @@ rm -rf ../src_dependencies && rm -rf ../build.zip
 
 echo "${BLUE}Building dependencies... 📦${NOCOLOR}"
 # it uses docker so dependencies are build from a linux image, resembling lambda real environment
-cd ..
 docker run -v "$PWD":/var/task "lambci/lambda:build-python3.8" /bin/sh -c "pip install -r requirements.txt -t src_dependencies/; exit"
-cd ./src_dependencies
+pushd src_dependencies
 zip ../build.zip -r .
-cd ..
+popd
 
 echo "${BLUE}Adding source code... 🌊${NOCOLOR}"
-cd ./src
+pushd src
 zip ../build.zip -u . -r
+popd
 
 echo "${GREEN}Done ✅${NOCOLOR}"
