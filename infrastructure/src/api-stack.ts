ead88fc380060677e40f1d034d57af62e654493d
alramalho <alexandre.ramalho.1998@gmail.com>
2023-03-19 23:44:19 +0100

    Decrease as apparently limit is 3008

--- infrastructure/src/api-stack.ts
+++ infrastructure/src/api-stack.ts
@@ -25,7 +25,7 @@ export class ApiStack extends cdk.Stack {
             handler: 'api.handler',
             runtime: lambda.Runtime.PYTHON_3_8,
             timeout: cdk.Duration.seconds(150),
-            memorySize: 4096,
+            memorySize: 3008,
             environment: {
                 OPENAI_API_KEY: props.openaiApiKey,
                 ENVIRONMENT: props.environment,
