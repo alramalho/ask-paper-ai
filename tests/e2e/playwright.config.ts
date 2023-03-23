73c3af62fa9e1dd9b4091868356c3f02b596825e
alramalho <alexandre.ramalho.1998@gmail.com>
2023-03-23 16:05:46 +0100

    Increase timeout for tests

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -14,13 +14,13 @@ require('dotenv').config()
 const config: PlaywrightTestConfig = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 120 * 1000,
+  timeout: 180 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 60 * 1000
+    timeout: 120 * 1000
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
