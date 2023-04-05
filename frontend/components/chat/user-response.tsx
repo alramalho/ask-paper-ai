7ee1a22b5191388f9ff241f11034cc14a0f22618
alramalho <alexandre.ramalho.1998@gmail.com>
2023-04-05 23:49:21 +0200

    Change UserMessage message

--- frontend/components/chat/user-response.tsx
+++ frontend/components/chat/user-response.tsx
@@ -7,8 +7,8 @@ const UserResponse = ({ text }: { text: string }) => {
         <Flex css={{ margin: '$6', gap: "$5", flexWrap: 'nowrap', overflow: 'visible', justifyContent: 'flex-end' }}>
             <Box css={{
                 textAlign: 'left',
-                backgroundColor: '$primaryLight',
-                border: '1px solid $primary',
+                backgroundColor: '$blue100',
+                border: '1px solid $blue500',
                 padding: '$6 $10',
                 borderRadius: '20px 20px 0 20px',
             }}>
