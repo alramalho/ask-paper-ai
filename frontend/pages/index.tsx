df6dcd69dbf2543ee3f015cbaa3d446d89ad4d48
alramalho <alexandre.ramalho.1998@gmail.com>
2023-04-06 10:39:05 +0200

    Have predefined actions opened

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -259,7 +259,7 @@ const Home = () => {
                 }> Ask </Button>
             </Flex>
             <Info text={"The chat interface does not support referencing to older messages yet! We are working on it :)"} />
-            <Collapse size="small" style={{ width: "100%" }}>
+            <Collapse size="small" style={{ width: "100%" }} defaultActiveKey={['2']}>
               <Panel header="🛠 Configuration" key="1">
                 <Flex css={{ gap: "$2", justifyContent: 'flex-start' }}>
                   <Switch bordered initialChecked checked={quoteChecked}
