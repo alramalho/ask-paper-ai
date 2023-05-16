2e6308e686b3823a63b7540ec39c0dc2eba631d8
alramalho <alexandre.ramalho.1998@gmail.com>
2023-05-16 13:05:04 +0200

    fix bug

--- frontend/components/next-auth-session-wrapper.tsx
+++ frontend/components/next-auth-session-wrapper.tsx
@@ -200,7 +200,7 @@ const NextAuthSessionWrapper = ({ children }: ChildrenOnlyProps) => {
           <p><a href="https://discord.gg/6rVU4hrc9f">Click here</a> to join us! </p>
           <br/>
           <p>If you believe this is an error, <Button type="text" onClick={() => signOut()}>click here</Button></p>
-          <p><small>> If the error persists, please open a support ticket</small></p>
+          <p><small> If the error persists, please open a support ticket</small></p>
         </AbsoluteCenter>
       )
     }
