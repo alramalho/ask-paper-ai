637de4ece66ae5bdcdebc39cb126d60f49489b48
alramalho <alexandre.ramalho.1998@gmail.com>
2023-07-03 15:22:47 +0200

    use hypercorn locally

--- backend/scripts/run_local.sh
+++ backend/scripts/run_local.sh
@@ -3,5 +3,5 @@
 set -e
 
 pushd src
-uvicorn api:app --reload --port 8000 --workers 1
-# hypercorn api:app --reload --bind 0.0.0.0:8000 --workers 1
+# uvicorn api:app --reload --port 8000 --workers 1
+hypercorn api:app --reload --bind 0.0.0.0:8000 --workers 1
