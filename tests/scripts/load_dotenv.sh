dbb41670dc5ddd4a52b3bc0f932197588d74dcce
alramalho <alexandre.ramalho.1998@gmail.com>
2023-07-05 10:57:18 +0200

    .env refactor

---
+++ tests/scripts/load_dotenv.sh
@@ -0,0 +1,6 @@
+export $( grep -vE "^(#.*|\s*)$" $1 )
+
+line_count=$(grep -v "^#" $1 | wc -l)
+GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
+echo "${GREEN}✅ Loaded $1 with $line_count env vars. ${NOCOLOR}"
