982e3f35b0bb7061c4f3e9e96e739497940b099b
alramalho <alexandre.ramalho.1998@gmail.com>
2023-07-12 08:53:32 +0200

    remove comments

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -7,112 +7,44 @@ var id = crypto.randomUUID();
 export const TEST_EMAIL = ((process.env.ENVIRONMENT ?? 'local') + "-" + id) + '@e2e.test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 
 const browsers = [
-  // {
-  //   name: 'chromium',
-  //   use: {
-  //     ...devices['Desktop Chrome'],
-  //   },
-  // },
   {
-    name: 'firefox',
+    name: 'chromium',
     use: {
-      ...devices['Desktop Firefox'],
+      ...devices['Desktop Chrome'],
     },
   },
-  //
-  // {
-  //   name: 'webkit',
-  //   use: {
-  //     ...devices['Desktop Safari'],
-  //   },
-  // },
-  //
-  // /* Test against mobile viewports. */
-  // {
-  //   name: 'Mobile Chrome',
-  //   use: {
-  //     ...devices['Pixel 5'],
-  //   },
-  // },
-  // {
-  //   name: 'Mobile Safari',
-  //   use: {
-  //     ...devices['iPhone 12'],
-  //   },
-  // },
-
-  /* Test against branded browsers. */
-  // {
-  //   name: 'Microsoft Edge',
-  //   use: {
-  //     channel: 'msedge',
-  //   },
-  // },
-  // {
-  //   name: 'Google Chrome',
-  //   use: {
-  //     channel: 'chrome',
-  //   },
-  // },
 ]
 process.env.CI && browsers.push({
-  name: 'chromium',
+  name: 'firefox',
   use: {
-    ...devices['Desktop Chrome'],
+    ...devices['Desktop Firefox'],
   },
 })
 const config: PlaywrightTestConfig = {
   testDir: './tests',
-  /* Maximum time one test can run for. */
   timeout: 180 * 1000,
   expect: {
-    /**
-     * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
-     */
     timeout: 120 * 1000,
   },
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: 0,
-  /* Opt out of parallel tests on CI. */
   workers: 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'github' : 'line',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'off',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
-
-  /* Configure projects for major browsers */
   projects: browsers,
 
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   outputDir: 'playwright-test-output/',
   maxFailures: 0,
-
-
 };
 
 export default config;
